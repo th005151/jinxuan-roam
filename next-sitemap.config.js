b05f742072f6725329da5c345e105d879af8d126
HuJ <rg005151@gmail.com>
2026-05-14 15:30:19 +0800

    feat(seo): add next-sitemap with /go exclusion

---
+++ next-sitemap.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000",
+  generateRobotsTxt: true,
+  exclude: ["/go/*"],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/go/"],
+      },
+    ],
+  },
+};

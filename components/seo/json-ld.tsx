0673f93da9bc491297e1b76c38e87e600e2d0760
HuJ <rg005151@gmail.com>
2026-05-14 13:55:27 +0800

    feat(seo): add JsonLd injection component

---
+++ components/seo/json-ld.tsx
@@ -0,0 +1,8 @@
+export function JsonLd<T extends object>({ data }: { data: T }) {
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+    />
+  );
+}

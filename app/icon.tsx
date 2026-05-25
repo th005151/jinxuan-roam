7938de7a94854de0f46f25834274166e31c5458c
HuJ <rg005151@gmail.com>
2026-05-26 01:16:19 +0800

    feat(brand): regenerate favicon + apple-icon as tea-leaf

--- app/icon.tsx
+++ app/icon.tsx
@@ -11,20 +11,33 @@ export default function Icon() {
         style={{
           width: "100%",
           height: "100%",
-          background: "#059669",
-          color: "#FFFFFF",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          fontFamily: "Inter, system-ui, sans-serif",
-          fontWeight: 800,
-          fontSize: 235,
-          letterSpacing: "-0.05em",
-          lineHeight: 1,
+          background: "#ECFDF5",
           borderRadius: 96,
         }}
       >
-        FX
+        {/* Tea-leaf mark */}
+        <svg
+          width="360"
+          height="360"
+          viewBox="0 0 32 32"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M6 26 Q 4 14 16 4 Q 28 14 26 26 Q 16 22 6 26 Z"
+            fill="#059669"
+          />
+          <path
+            d="M8 24 Q 16 16 24 6"
+            stroke="white"
+            strokeWidth="1.4"
+            strokeLinecap="round"
+            fill="none"
+            opacity="0.7"
+          />
+        </svg>
       </div>
     ),
     size
